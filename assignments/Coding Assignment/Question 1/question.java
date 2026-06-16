89b1cacbe8dacaca7bbe69c4887bcce93a44979a
Yash Sharma <yshsharma90913@gmail.com>
2026-06-16 15:02:28 +0530

    [C2G] Submit: Coding Assignment - Question 1

---
+++ assignments/Coding Assignment/Question 1/question.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int b = sc.nextInt();
+        if(b<20) System.out.println("Charge Soon");
+        else System.out.println("Battery OK");
+        
+    }
+}
